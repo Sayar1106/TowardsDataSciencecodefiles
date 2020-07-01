55e98a0cb3c007f86f77ab550151574d439b3bf4
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-01 19:28:39 +0530

    Adding comments

--- Kmeansfromscratch/kmeans.py
+++ Kmeansfromscratch/kmeans.py
@@ -19,6 +19,7 @@ class Kmeans:
 
     def _cluster_means(self, X, clusters):
         m, n = X.shape[0], X.shape[1]
+        # Extra column to store cluster ids
         temp = np.zeros((m, n + 1))
         temp[:, :n], temp[:, n] = X, clusters
         result = np.zeros((self.k, n))
@@ -26,23 +27,30 @@ class Kmeans:
             subset = temp[np.where(temp[:, -1] == i), :n]
             if subset[0].shape[0] > 0:
                 result[i] = np.mean(subset[0], axis=0)
+            # Choose random data point if a cluster does not 
+            # have any data associated with it
             else:
                 result[i] = X[np.random.choice(X.shape[0], 1, replace=True)]
 
         return result
 
     def _compute_cluster(self, x):
+        # Computes closest means to a data point x
         return min(range(self.k), key=lambda i: np.linalg.norm(x - self.means[i])**2)
 
     def fit(self, X, num_iterations=None):
         m = X.shape[0]
+        # Initialize clusters
         initial_clusters = self._init_centroid(m)
         new_clusters = np.zeros(initial_clusters.shape)
         with tqdm.tqdm(itertools.count()) as t:
             for _ in t:
+                # Compute cluster means
                 self.means = self._cluster_means(X, initial_clusters)
                 for i in range(m):
+                    # Assign new cluster ids
                     new_clusters[i] = self._compute_cluster(X[i])
+                # Check for data points that have switched cluster ids.
                 count_changed = (new_clusters != initial_clusters).sum()
                 if count_changed == 0:
                     break
